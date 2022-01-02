d1ffc2fb82c532f79c7e68df8804596813094ced
Philo96 <94920732+Philo96@users.noreply.github.com>
2022-01-02 18:04:49 +0200

    Create Band'sNameGenerator.py

---
+++ Band'sNameGenerator.py
@@ -0,0 +1,8 @@
+print("Welcome To Band Name Generator.")
+
+name_of_city =str(input("What's Your City Name?!!\n"))
+name_of_rabit = str(input("What's Your Rabit Name?!!\n"))
+name_of_band = name_of_city+"'s "+name_of_rabit
+print(name_of_band)
+
+print("Thank's For Using Me...")
